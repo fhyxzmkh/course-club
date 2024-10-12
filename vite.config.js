3d1f49fc5b499693895ed3983cf9dd596226f02b
fhyxzmkh <2289960268@qq.com>
2024-10-12 23:40:58 +0800

    Create a project

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8888, // 修改为你想要的端口号
+  },
+});
